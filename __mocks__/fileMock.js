d6c9ae5a0a8c0980570c6c77d3918467254b3d48
Shishir Anshuman <shishir.anshuman@causecode.com>
2016-12-16 19:03:30 +0530

    Added configuration for jest to handle css, images and other asset files.

---
+++ __mocks__/fileMock.js
@@ -0,0 +1,7 @@
+/**
+ * We need to tell Jest how to handle CSS files. This stub is the module we tell Jest to use instead of asset files such as images files.
+ * See moduleNameMapper in package.json
+ * For information visit: http://facebook.github.io/jest/docs/tutorial-webpack.html
+ */
+
+module.exports = 'test-file-stub';
---
+++ __mocks__/styleMock.js
@@ -0,0 +1,7 @@
+/**
+ * We need to tell Jest how to handle CSS files. This stub is the module we tell Jest to use instead of CSS or Less files.
+ * See moduleNameMapper in package.json
+ * For information visit: http://facebook.github.io/jest/docs/tutorial-webpack.html
+ */
+
+module.exports = {};
