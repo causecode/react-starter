c0c1e76c45f330f37dcf7fd5e20616da4e18aa33
Shishir Anshuman <shishir.anshuman@causecode.com>
2017-01-19 12:31:58 +0530

    Fixed comment in fileMock.js

--- __mocks__/fileMock.js
+++ __mocks__/fileMock.js
@@ -1,5 +1,5 @@
 /**
- * We need to tell Jest how to handle CSS files. This stub is the module we tell Jest to use instead of asset files such as images files.
+ * This stub is the module we tell Jest to use instead of asset files such as images.
  * See moduleNameMapper in package.json
  * For information visit: http://facebook.github.io/jest/docs/tutorial-webpack.html
  */
